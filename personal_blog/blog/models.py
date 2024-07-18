953c480d3061445f978ab0a5f973e3a336b29422
Will <willj4945@gmail.com>
2024-07-17 22:01:42 -0400

    Started user model.

--- personal_blog/blog/models.py
+++ personal_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_lengtth=100)
+
+# Finish creating the user model.
+# Figure out best pratice for password management,
